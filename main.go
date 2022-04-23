b6133e6d4868d97dd6b663cc6213df42ae1e27a1
aboxofsox <idkwthfml@gmail.com>
2022-04-23 00:21:16 -0500

    removed dead code

--- main.go
+++ main.go
@@ -3,11 +3,5 @@ package main
 import goloc "aboxofsox/goloc/pkg"
 
 func main() {
-	// files := goloc.Load()
-
-	// for k, v := range files {
-	// 	fmt.Printf("%s: %d\n", k, v)
-	// }
-
 	goloc.Printer(goloc.Load())
 }
